153ae9c090a51343f06dd9a4ad87c728c887c1a3
Robert Lowe <robertmlowe@rmlowe.com>
2020-10-03 19:10:38 +0800

    [OPTIONAL] Assignment Solution

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,45 +4,31 @@ var output = document.querySelector('#output');
 
 button.addEventListener('click', function () {
   // Create a new Promise here and use setTimeout inside the function you pass to the constructor
-  // var promise = new Promise(function (resolve, reject) {
-  //   setTimeout(function () {
-  //     // Resolve the following URL: https://swapi.co/api/people/1
-  //     resolve('https://swapi.dev/api/people/1');
-  //   }, 3000);
-  // }).then(function (url) {
-  //   return fetch(url);
-  // }).then(function (response) {
-  //   return response.json();
-  // }).then(function (data) {
-  //   output.innerHTML = data.name;
-  // });
-
   var promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
+    setTimeout(function () { // <- Store this INSIDE the Promise you created!
       // Resolve the following URL: https://swapi.co/api/people/1
       resolve('https://httpbin.org/puts');
     }, 3000);
-  }).then(function (url) {
-    return fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      mode: 'cors',
-      body: JSON.stringify({ person: { name: 'Max', age: 28 } })
+  })
+    .then(function (url) {
+      return fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({ person: { name: 'Max', age: 28 } })
+      });
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      output.innerHTML = data.json.person.name;
+    })
+    .catch(function (err) {
+      console.log(err);
     });
-  }).then(function (response) {
-    return response.json();
-  }).then(function (data) {
-    output.innerHTML = data.json.person.name;
-  }).catch(function (err) {
-    output.innerHTML = 'Error: ' + err;
-  });
-
-  // setTimeout(function () { // <- Store this INSIDE the Promise you created!
-  //   // Resolve the following URL: https://swapi.co/api/people/1
-  // }, 3000);
 
   // Handle the Promise "response" (=> the value you resolved) and return a fetch()
   // call to the value (= URL) you resolved (use a GET request)
